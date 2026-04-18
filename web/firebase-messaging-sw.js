d18761c9981355544be9c48bb1e574f2afcdfa4f
Vetra <suadatbiniqbal@outlook.com>
2026-04-18 14:04:10 +0530

    Update firebase-messaging-sw.js

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,12 +1,5 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyApubO067JYW1ih6AUii86Nu0jC6WpFcVI',
-  appId: '1:227933260927:web:d0902085d37eb89738034a',
-  messagingSenderId: '227933260927',
-  projectId: 'harmber',
-  storageBucket: 'harmber.firebasestorage.app'
-});
 
 const messaging = firebase.messaging();
