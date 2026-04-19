7d45fd716a1730daaf821024fd22e9ba4e58bdd2
Vetra <suadatbiniqbal@outlook.com>
2026-04-19 15:38:45 +0530

    api

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,11 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 
-//Firebase Details 
-        
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: "YOUR_API_KEY",
+    appId: "YOUR_APP_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+  );
 }
