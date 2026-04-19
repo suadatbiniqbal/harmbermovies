73e38ea9fc30cbb30de660c70936728a3ec85235
Vetra <suadatbiniqbal@outlook.com>
2026-04-19 10:23:51 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,5 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 
-/// Placeholder Firebase options.
-/// Run `flutterfire configure` to generate real values.
-class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform => const FirebaseOptions(
-        apiKey: 'AIzaSyApubO067JYW1ih6AUii86Nu0jC6WpFcVI',
-        appId: '1:227933260927:android:d0902085d37eb89738034a',
-        messagingSenderId: '227933260927',
-        projectId: 'harmber',
-        storageBucket: 'harmber.firebasestorage.app',
-      );
+//Firebase Details 
+        
 }
